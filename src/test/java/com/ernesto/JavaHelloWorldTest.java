a75bf98a08f8a67d5c2c438c711626440dfd4a15
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-08-02 23:06:21 -0500

    add JavaHelloWorldTest class with junit4 annotations

---
+++ src/test/java/com/ernesto/JavaHelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.ernesto;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        
+    }
+}
