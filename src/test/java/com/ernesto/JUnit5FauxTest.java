7f435b2431db26d923e879863984f2fb2a70ea0c
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-08-08 00:32:52 -0500

    add JUnit5FauxTest class to test JUnit5 in parallel with JUnit4 on class JavaHelloWorldTest

---
+++ src/test/java/com/ernesto/JUnit5FauxTest.java
@@ -0,0 +1,10 @@
+package com.ernesto;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5FauxTest {
+    @Test
+    void someTestforJUNit5() {
+        System.out.println("I Ran...");
+    }
+}
