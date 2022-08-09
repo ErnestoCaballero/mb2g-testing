db91ed1cc5dc542957d45346a9ecee7be6fa2557
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-08-08 23:41:17 -0500

    add IntegrationTest class JavaHelloWorldIT following name conventions

--- src/test/java/com/ernesto/JavaHelloWorldIT.java
+++ src/test/java/com/ernesto/JavaHelloWorldIT.java
@@ -1,2 +1,10 @@
-package com.ernesto;public class JavaHelloWorldIT {
+package com.ernesto;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran");
+    }
 }
